c66a4173ee136e6fe4b858fe80050172c7b17e93
ksharma <karnveer.alliance@gmail.com>
2022-01-26 22:20:59 +0530

    Change add from class method to instance method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,7 @@
 # String calculator
 
 class StringCalculator
-  def self.add(string_numbers)
+  def add(string_numbers)
     return 0 if string_numbers.empty?
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,10 +2,13 @@
 require "string_calculator"
 
 describe StringCalculator do
+
+  subject(:string_calculator) {described_class.new}
+  
   describe ".add" do
     context "empty string" do
       it "returns 0" do
-        expect(StringCalculator.add("")).to eql(0)
+        expect(string_calculator.add("")).to eql(0)
       end
     end
   end
