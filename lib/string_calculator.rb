8781772d11ffba255746c42bb4a5790444c39f66
ksharma <karnveer.alliance@gmail.com>
2022-01-26 22:35:30 +0530

    Update method to calculate 1 number in string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,5 +3,12 @@
 class StringCalculator
   def add(string_numbers)
     return 0 if string_numbers.empty?
+
+    string_to_integer(string_numbers)
   end
+
+  private
+    def string_to_integer(string_num)
+      string_num.to_i
+    end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,11 +4,21 @@ require "string_calculator"
 describe StringCalculator do
 
   subject(:string_calculator) {described_class.new}
-  
+
   describe ".add" do
     context "empty string" do
       it "returns 0" do
-        expect(string_calculator.add("")).to eql(0)
+        expect(string_calculator.add('')).to eql(0)
+      end
+    end
+
+    context "with 1 number" do
+      it "returns 5" do
+        expect(string_calculator.add('5')).to eql(5)
+      end
+
+      it "returns 8" do
+        expect(string_calculator.add('5')).to eql(5)
       end
     end
   end
